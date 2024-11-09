0c31c015620332a10710c80bc8c71f6283f625f2
annagorsh <annagorshhh@gmail.com>
2024-11-09 06:41:11 +0300

    создание pull request

---
+++ links.py
@@ -0,0 +1,22 @@
+MAIN_URL = "https://stellarburgers.nomoreparties.site/"
+
+LOGIN_URL = MAIN_URL + "login"
+
+REGISTER_URL = MAIN_URL + "register"
+
+FORGOT_PASSWORD_URL = MAIN_URL + "forgot-password"
+
+RESET_PASSWORD_URL = MAIN_URL + "reset-password"
+
+PROFILE_PAGE_URL = MAIN_URL + "account/profile"
+
+ORDER_FEED_URL = MAIN_URL + "feed"
+
+ORDER_HISTORY_URL = MAIN_URL + "account/order-history"
+
+#Ссылки для API-запросов
+API_URL = "https://stellarburgers.nomoreparties.site/api/"
+
+CREATE_USER_URL = API_URL+"auth/register"
+
+AUTH_USER_URL = API_URL+"auth/user"
